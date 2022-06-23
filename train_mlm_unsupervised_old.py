fccded1883d5fd432e408240e767b9303cea4019
kshll6 <kshll6@hotmail.com>
2022-06-23 10:30:35 +0200

    add transformers trainer

--- train_mlm_unsupervised_old.py
+++ train_mlm_unsupervised_old.py
@@ -1,44 +1,56 @@
-import json
 import os
 from datetime import datetime
 
-import torch
+import numpy as np
+
+from modelling_utils.mlm_modelling import MLMUnsupervisedModelling
 from transformers import AutoTokenizer, Trainer, \
     TrainingArguments, AutoModelForMaskedLM, DataCollatorForWholeWordMask, \
     DataCollatorForLanguageModeling
 
-from data_utils.preprocess_public_scraped_data import split_to_sentences, split_train_val, \
-    TokenizedSentencesDataset
-from local_constants import OUTPUT_DIR, DATA_DIR, CONFIG_DIR
-from utils.input_args import create_parser
+from local_constants import MODEL_DIR
+from utils.input_args import MLMArgParser
+import os
+from datetime import datetime
+
+import numpy as np
+from transformers import AutoTokenizer, Trainer, \
+    TrainingArguments, AutoModelForMaskedLM, DataCollatorForWholeWordMask, \
+    DataCollatorForLanguageModeling
+
+from local_constants import MODEL_DIR
+from modelling_utils.mlm_modelling import MLMUnsupervisedModelling
+from utils.input_args import MLMArgParser
+
 os.environ["WANDB_DISABLED"] = "true"
+from datasets import load_metric
 
-print(torch.cuda.is_available())
+mlm_parser = MLMArgParser()
+args = mlm_parser.parser.parse_args()
+# args.model_name = 'Geotrend/distilbert-base-da-cased'
+# args.train_data = 'train_200.json'
+# args.logging_steps = 20
+# args.evaluate_steps = 20
+# args.save_steps = 20
 
 
-parser = create_parser()
-args = parser.parse_args()
+mlm_modelling = MLMUnsupervisedModelling(args=args)
 
 # get data
-sentences = split_to_sentences(data_path=os.path.join(DATA_DIR, args.data_file))
-
-train_sentences, dev_sentences = split_train_val(sentences=sentences)
+mlm_modelling.load_data()
 
 # Load the model
 model = AutoModelForMaskedLM.from_pretrained(args.model_name)
 tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
 output_name = f'{args.model_name.replace("/", "_")}-{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
-output_dir = os.path.join(OUTPUT_DIR, output_name)
-
-with open(os.path.join(CONFIG_DIR, output_name), 'w', encoding='utf-8') as f:
-    json.dump(args.__dict__, f, indent=2)
+output_dir = os.path.join(MODEL_DIR, output_name)
+mlm_modelling.save_config(output_dir=output_dir, args=args)
 
 print("Save checkpoints to:", output_dir)
 
-train_dataset = TokenizedSentencesDataset(train_sentences, tokenizer, args.max_length)
-dev_dataset = TokenizedSentencesDataset(dev_sentences, tokenizer, args.max_length,
-                                        cache_tokenization=True) if len(dev_sentences) > 0 else None
+train_data_wrapped = mlm_modelling.tokenize_and_wrap_data(data=mlm_modelling.train_data)
+eval_data_wrapped = mlm_modelling.tokenize_and_wrap_data(data=mlm_modelling.eval_data)
 
 if args.whole_word_mask:
     data_collator = DataCollatorForWholeWordMask(tokenizer=tokenizer, mlm=True,
@@ -47,27 +59,39 @@ else:
     data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=True,
                                                     mlm_probability=args.mlm_prob)
 
+metric = load_metric('accuracy')
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
+
 training_args = TrainingArguments(
-    output_dir=os.path.join(OUTPUT_DIR, output_name),
+    output_dir=os.path.join(MODEL_DIR, output_name),
     overwrite_output_dir=True,
     num_train_epochs=args.epochs,
     evaluation_strategy='steps',
-    per_device_train_batch_size=args.batch_size,
+    do_eval=True,
+    per_device_train_batch_size=args.train_batch_size,
     eval_steps=args.evaluate_steps,
     save_steps=args.save_steps,
     logging_steps=args.logging_steps,
     save_total_limit=1,
-    prediction_loss_only=True,
+    warmup_steps=500,
+    # prediction_loss_only=True,
     fp16=args.use_fp16,
-    push_to_hub=False
+    push_to_hub=False,
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
     data_collator=data_collator,
-    train_dataset=train_dataset,
-    eval_dataset=dev_dataset
+    train_dataset=train_data_wrapped,
+    eval_dataset=eval_data_wrapped,
+    compute_metrics=compute_metrics,
 )
 
 print("Save tokenizer to:", output_dir)
@@ -76,6 +100,6 @@ tokenizer.save_pretrained(output_dir)
 trainer.train()
 
 print("Save model to:", output_dir)
-model.save_pretrained(output_dir)
-
+# model.save_pretrained(output_dir)
+trainer.save_model()
 print("Training done")
