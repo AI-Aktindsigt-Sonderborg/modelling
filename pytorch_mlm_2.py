37b031f40234a8665ff393bbc9bd6713453e2314
kshll6 <kshll6@hotmail.com>
2022-06-24 13:42:23 +0200

    modify pytorch_mlm_2.py for comparison

--- pytorch_mlm_2.py
+++ pytorch_mlm_2.py
@@ -105,8 +105,8 @@ for epoch in range(40):
         # np.save('data/labels_manual', batch['labels'].cpu().detach().numpy())
 
        # -100 index = padding token
-        loss = loss_fct(outputs.logits.cpu().view(-1, 119547), batch['labels'].cpu().view(-1)).item()
-        CE_losses.append(loss)
+        loss = loss_fct(outputs.logits.cpu().view(-1, 119547), batch['labels'].cpu().view(-1))
+        CE_losses.append(loss.item())
         # print(outputs.loss.item())
         # loss = criterion(outputs, batch['labels'])
         model_losses.append(outputs.loss.item())
