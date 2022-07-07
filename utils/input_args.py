1462a18df60a47a7c950a736694729e268180d84
kshll6 <kshll6@hotmail.com>
2022-07-07 18:31:50 +0200

    change default args

--- utils/input_args.py
+++ utils/input_args.py
@@ -22,7 +22,7 @@ class MLMArgParser:
                                   help="foundation model from huggingface")
         model_params.add_argument("--save_config", type=lambda x: bool(strtobool(x)), default=True,
                                   help="Whether to save input args to file")
-        model_params.add_argument("--replace_head", type=lambda x: bool(strtobool(x)), default=False,
+        model_params.add_argument("--replace_head", type=lambda x: bool(strtobool(x)), default=True,
                                   help="Whether to replace bert head")
         model_params.add_argument("--save_model_at_end", type=lambda x: bool(strtobool(x)), default=True,
                                   help="Whether to save final model after training.")
@@ -42,7 +42,7 @@ class MLMArgParser:
                                      help="Weight decay")
         training_params.add_argument("--use_fp16", type=lambda x: bool(strtobool(x)), default=False,
                                      help="Set to True, if your GPU supports FP16 operations")
-        training_params.add_argument("--train_batch_size", type=int, default=2,
+        training_params.add_argument("--train_batch_size", type=int, default=8,
                                      help="Batch size specifies the sample size of which the gradients are "
                                           "computed. Depends on memory available")
         training_params.add_argument("--whole_word_mask", type=lambda x: bool(strtobool(x)), default=False,
@@ -51,9 +51,9 @@ class MLMArgParser:
                                      help="Probability that a word is replaced by a [MASK] token")
         training_params.add_argument("--device", type=str, default='cuda',
                                      help="device to train on, can be either 'cuda' or 'cpu'")
-        training_params.add_argument("--freeze_layers", type=lambda x: bool(strtobool(x)), default=False,
+        training_params.add_argument("--freeze_layers", type=lambda x: bool(strtobool(x)), default=True,
                                      help="whether to freeze all bert layers until freeze_layers_n_steps is reached")
-        training_params.add_argument("--freeze_layers_n_steps", type=int, default=3000,
+        training_params.add_argument("--freeze_layers_n_steps", type=int, default=10000,
                                      help="number of steps to train head only")
         training_params.add_argument("--lr_freezed", type=float, default=0.0005,
                                      help="number of steps to train head only")
@@ -61,12 +61,12 @@ class MLMArgParser:
                                      help="number of steps to train head only")
         training_params.add_argument("--lr", type=float, default=0.00005,
                                      help="Learning rate")
-        training_params.add_argument("--lr_warmup_steps", type=int, default=1000,
+        training_params.add_argument("--lr_warmup_steps", type=int, default=4000,
                                      help="warmup learning rate - set to 1 if no warmup")
-        training_params.add_argument("--lr_start_decay", type=int, default=3000,
+        training_params.add_argument("--lr_start_decay", type=int, default=40000,
                                      help="after which step to start decaying learning rate ")
-        training_params.add_argument("--grad_accum_steps", type=int, default=1,
-                                     help="Number of updates steps to accumulate the gradients for, before performing a backward/update pass.")
+        # training_params.add_argument("--grad_accum_steps", type=int, default=1,
+        #                              help="Number of updates steps to accumulate the gradients for, before performing a backward/update pass.")
         # training_params.add_argument("--start_lr", type=float, default=0.00001,
         #                              help="Learning rate")
         # training_params.add_argument("--end_lr", type=float, default=0.00001,
@@ -76,9 +76,9 @@ class MLMArgParser:
     def add_eval_params(self):
         eval_params = self.parser.add_argument_group('evaluation')
         # ToDo: evaluate steps must be smaller than number of steps in each epoch
-        eval_params.add_argument("--evaluate_steps", type=int, default=150,
+        eval_params.add_argument("--evaluate_steps", type=int, default=600,
                                  help="evaluate model accuracy after number of steps")
-        eval_params.add_argument("--logging_steps", type=int, default=100,
+        eval_params.add_argument("--logging_steps", type=int, default=600,
                                  help="Log model accuracy after number of steps")
         eval_params.add_argument("--save_steps", type=int, default=None,
                                  help="save checkpoint after number of steps")
@@ -93,8 +93,8 @@ class MLMArgParser:
     def add_dp_params(self):
         dp_params = self.parser.add_argument_group('dp')
         dp_params.add_argument("--epsilon", type=float, default=1, help="privacy parameter epsilon")
-        dp_params.add_argument("--delta", type=float, default=0.00002,
-                               help="privacy parameter delta")
+        dp_params.add_argument("--delta", type=float, default=4.6e-05,
+                               help="privacy parameter delta. Usually a good delta is 1/len(train)")
         dp_params.add_argument("--max_grad_norm", type=float, default=1.2,
                                help="maximum norm to clip gradient")
         dp_params.add_argument("--lot_size", type=int, default=8,
