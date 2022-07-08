6491fe69791244ac49e72c013635bcbc63aaad1f
kshll6 <kshll6@hotmail.com>
2022-07-08 14:33:47 +0200

    add bash to run multiple models

--- run_multiple_models.sh
+++ run_multiple_models.sh
@@ -8,7 +8,7 @@ python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_da
 python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 200 &
 
 python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --lot_size 16 &
-python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --max_length 32 &
-python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --max_length 64 &
-python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --max_length 256 &
-python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --max_length 512
+python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --lot_size 32 &
+python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --lot_size 64 &
+python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --lot_size 256 &
+python train_mlm_unsupervised_dp.py --train_data train_new_scrape.json --eval_data val_new_scrape.json --epsilon 1 --lot_size 512
