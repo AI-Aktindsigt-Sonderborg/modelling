5350d8b003bd0dd250bef65cfe218300118df2d2
kshll6 <kshll6@hotmail.com>
2022-07-14 13:28:17 +0200

    add data subsets

--- run_multiple_models.sh
+++ run_multiple_models.sh
@@ -7,11 +7,11 @@
 #python train_mlm_unsupervised_dp.py  --epsilon 16 &&
 #python train_mlm_unsupervised_dp.py  --epsilon 200 &&
 
-python train_mlm_unsupervised.py --max_length 64 --train_data train_1000.json --epsilon 4 --lot_size 36 --evaluate_steps 100 &&
-python train_mlm_unsupervised.py --max_length 64 --train_data train_5000.json --epsilon 4 --lot_size 36 --evaluate_steps 500 &&
-python train_mlm_unsupervised.py --max_length 64 --train_data train_10000.json --epsilon 4 --lot_size 36 --evaluate_steps 1000 &&
+#python train_mlm_unsupervised.py --max_length 64 --train_data train_1000.json --epsilon 4 --lot_size 36 --evaluate_steps 100 && # 400 epochs
+python train_mlm_unsupervised.py --max_length 64 --train_data train_5000.json --epsilon 4 --lot_size 36 --evaluate_steps 500 --epochs 80 &&
+python train_mlm_unsupervised.py --max_length 64 --train_data train_10000.json --epsilon 4 --lot_size 36 --evaluate_steps 1000 --epochs 40
 
-python train_mlm_unsupervised.py --max_length 16 --epsilon 4 --lot_size 36 &&
-python train_mlm_unsupervised.py --max_length 32 --epsilon 4 --lot_size 36 &&
-python train_mlm_unsupervised.py --max_length 128 --epsilon 4 --lot_size 36 &&
-python train_mlm_unsupervised.py --max_length 256 --epsilon 4 --lot_size 36
+#python train_mlm_unsupervised.py --max_length 16 --epsilon 4 --lot_size 36 &&
+#python train_mlm_unsupervised.py --max_length 32 --epsilon 4 --lot_size 36 &&
+#python train_mlm_unsupervised.py --max_length 128 --epsilon 4 --lot_size 36 &&
+#python train_mlm_unsupervised.py --max_length 256 --epsilon 4 --lot_size 36
