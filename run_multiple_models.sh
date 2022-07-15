8bf4de1db85507c4b42bdf53e08e04d36d391089
kshll6 <kshll6@hotmail.com>
2022-07-15 16:11:06 +0200

    add internal_data_preprocessing.py

--- run_multiple_models.sh
+++ run_multiple_models.sh
@@ -7,7 +7,7 @@
 #python train_mlm_unsupervised_dp.py  --epsilon 16 &&
 #python train_mlm_unsupervised_dp.py  --epsilon 200 &&
 
-#python train_mlm_unsupervised.py --max_length 64 --train_data train_1000.json --epsilon 4 --lot_size 36 --evaluate_steps 100 && # 400 epochs
+python train_mlm_unsupervised.py --max_length 64 --train_data train_1000.json --epsilon 4 --lot_size 36 --evaluate_steps 100 --epochs 400 &&
 python train_mlm_unsupervised.py --max_length 64 --train_data train_5000.json --epsilon 4 --lot_size 36 --evaluate_steps 500 --epochs 80 &&
 python train_mlm_unsupervised.py --max_length 64 --train_data train_10000.json --epsilon 4 --lot_size 36 --evaluate_steps 1000 --epochs 40
 
--- setup.py
+++ setup.py
@@ -33,3 +33,8 @@ if not os.path.exists('data/new_scrape'):
     # Create a new directory because it does not exist
     os.makedirs('data/new_scrape')
     print("Create new scrape data dir!")
+
+if not os.path.exists('data/internal_data'):
+    # Create a new directory because it does not exist
+    os.makedirs('data/internal_data')
+    print("Create internal data dir!")
