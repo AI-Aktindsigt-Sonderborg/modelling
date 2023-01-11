2a10e99e1185731839e8cefb9637749dc8ce36e4
kshll6 <kshll6@hotmail.com>
2023-01-11 12:05:00 +0100

    change concat_model_metrics.py

--- concat_model_metrics.py
+++ concat_model_metrics.py
@@ -11,6 +11,7 @@ def all_metrics_to_csv():
     for model_name in os.listdir(MODEL_DIR):
         metrics_file = os.path.join(MODEL_DIR,
                                     model_name, 'metrics', 'key_metrics.json')
+
         if os.path.isfile(metrics_file):
             data = read_json(metrics_file)
 
@@ -28,19 +29,27 @@ def all_metrics_to_csv():
                 data['lot_size'] = None
             if 'best_metrics' in data.keys():
                 data['loss'] = data['best_metrics']['loss']['score']
-                data['acc'] = data['best_metrics']['acc']['score']
+                if 'acc' in data['best_metrics'].keys():
+                    data['accuracy'] = data['best_metrics']['acc']['score']
+                else:
+                    data['accuracy'] = data['best_metrics']['accuracy']['score']
+
                 if 'f1' in data['best_metrics'].keys():
                     data['f_1'] = data['best_metrics']['f1']['score']
                 else:
                     data['f_1'] = data['best_metrics']['f_1']['score']
+            else:
+                data['accuracy'] = data['acc']
+                data.pop('acc', None)
 
-                data.pop('best_metrics', None)
+            data.pop('best_metrics', None)
 
             all_data.append(data)
 
     df = pd.DataFrame.from_records(all_data)
 
     df.to_csv('metadata/model_metrics.csv', sep='\t')
+    df.to_excel('metadata/model_metrics.xlsx', index=False)
 
 
 all_metrics_to_csv()
