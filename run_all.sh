051353046a72310fbd93387da291f4d39326ffe8
kshll6 <kshll6@hotmail.com>
2023-03-23 16:42:40 +0100

    add ner models to run ner

--- run_all.sh
+++ run_all.sh
@@ -2,6 +2,10 @@
 # declare -i count=`jq -r '.mlm | length' model_tests.json`
 # declare -i count=$count-1
 
+for i in {0..5}
+do
+    `jq -r '.ner['$i']' model_tests.json`
+done
 
 for i in {0..5}
 do
@@ -12,9 +16,3 @@ for i in {0..5}
 do
     `jq -r '.sc['$i']' model_tests.json`
 done
-
-for i in {0..5}
-do
-    `jq -r '.ner['$i']' model_tests.json`
-done
-
