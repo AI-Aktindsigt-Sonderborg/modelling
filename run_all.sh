2f4e69c815a556e25037184f93fdeff4ac431168
kshll6 <kshll6@hotmail.com>
2023-03-23 15:31:56 +0100

    add sc models to run all

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-declare -i count=`jq -r '.mlm | length' model_tests.json`
-declare -i count=$count-1
+# declare -i count=`jq -r '.mlm | length' model_tests.json`
+# declare -i count=$count-1
 
 for i in {0..5}
 do
     `jq -r '.mlm['$i']' model_tests.json`
+done
+
+for i in {0..5}
+do
+    `jq -r '.sc['$i']' model_tests.json`
 done
--- sc/train.py
+++ sc/train.py
@@ -1,11 +1,27 @@
 # pylint: disable=protected-access
 """Main script to train a sequence-classification model with custom train
 loop"""
+import logging
 import sys
 
 from sc.modelling_utils.input_args import SequenceModellingArgParser
 from sc.modelling_utils.sequence_classification import \
     SequenceClassificationDP, SequenceClassification
+
+logger = logging.getLogger('MLM model log')
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler('model_log.log')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 sc_parser = SequenceModellingArgParser()
 args = sc_parser.parser.parse_args()
@@ -20,19 +36,33 @@ LABELS = list(label_dict)
 args.labels = LABELS
 
 if args.differential_privacy:
+    sc_modelling = SequenceClassificationDP(args=args)
     if not ((args.lot_size > args.train_batch_size)
             and (args.lot_size % args.train_batch_size == 0)):
+        logger.warning(
+            f'SC model: {sc_modelling.args.output_name} - '
+            f'{sc_parser.parser._option_string_actions["--lot_size"].help}')
+
         print(sc_parser.parser._option_string_actions['--lot_size'].help)
         print('exiting - try again')
         sc_parser.parser.exit()
     elif not args.freeze_embeddings:
         print(sc_parser.parser._option_string_actions[
                   '--freeze_embeddings'].help)
+        logger.error(
+            f'SC model: {sc_modelling.args.output_name} - '
+            f'{sc_parser.parser._option_string_actions["--freeze_embeddings"].help}')
         print('exiting - try again')
         sc_parser.parser.exit()
 
-    sc_modelling = SequenceClassificationDP(args=args)
 else:
     sc_modelling = SequenceClassification(args=args)
 
-sc_modelling.train_model()
+try:
+    logger.info(f"Training SC model {sc_modelling.args.output_name}")
+    sc_modelling.train_model()
+    logger.info(
+        f'SC model {sc_modelling.args.output_name} trained succesfully')
+except Exception as ex:
+    logger.error(f'SC model {sc_modelling.args.output_name} failed:\n{ex}')
+
