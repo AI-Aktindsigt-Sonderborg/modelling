1dbc0fe07ac908fc1b13c560c08279c40f463ccd
kshll6 <kshll6@hotmail.com>
2023-03-24 14:28:44 +0100

    subset for ner test

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -4,15 +4,15 @@
 
 for i in {0..5}
 do
-    `jq -r '.ner['$i']' model_tests.json`
+    `jq -r '.ner['$i']' tests/model_tests.json`
 done
 
 for i in {0..5}
 do
-    `jq -r '.mlm['$i']' model_tests.json`
+    `jq -r '.mlm['$i']' tests/model_tests.json`
 done
 
 for i in {0..5}
 do
-    `jq -r '.sc['$i']' model_tests.json`
+    `jq -r '.sc['$i']' tests/model_tests.json`
 done
