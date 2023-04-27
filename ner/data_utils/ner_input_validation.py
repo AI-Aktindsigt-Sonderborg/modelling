3af9ef4e1715d81fae445876cfeff77e4099fa27
kshll6 <kshll6@hotmail.com>
2023-04-27 14:21:38 +0200

    add annotation error count

--- ner/data_utils/ner_input_validation.py
+++ ner/data_utils/ner_input_validation.py
@@ -6,6 +6,7 @@ filename_bilou = "bilou_output_user_annotated"
 data = read_jsonlines(DATA_DIR, filename)
 data_bilou = read_jsonlines(DATA_DIR, filename_bilou)
 error_count = 0
+annotation_errors = 0
 for i, obs in enumerate(data):
     try:
         assert isinstance(obs['id'], int)
@@ -17,20 +18,22 @@ for i, obs in enumerate(data):
         k = 0
         for page_num in list(obs['text_annotation']):
             for j, annotation in enumerate(obs['text_annotation'][page_num]):
-                assert isinstance(annotation, dict)
-                assert isinstance(annotation['annotation']['id'], int)
-                assert isinstance(annotation['annotation']['state'], str)
-                assert isinstance(annotation['annotation']['content'], str)
-                assert isinstance(annotation['annotation']['annotation'], str)
-                assert isinstance(annotation['annotation']['start'], int)
-                assert isinstance(annotation['annotation']['end'], int)
-                assert isinstance(annotation['annotation']['created'], str)
-                assert isinstance(annotation['annotation']['category']['unsure'], bool)
-                assert isinstance(annotation['annotation']['category']['guid'], str)
-                index = obs['pdf_text'][k][annotation['annotation']['start']:annotation['annotation']['end']]
-                content = annotation['annotation']['content']
-                assert index == content
-
+                try:
+                    assert isinstance(annotation, dict)
+                    assert isinstance(annotation['annotation']['id'], int)
+                    assert isinstance(annotation['annotation']['state'], str)
+                    assert isinstance(annotation['annotation']['content'], str)
+                    assert isinstance(annotation['annotation']['annotation'], str)
+                    assert isinstance(annotation['annotation']['start'], int)
+                    assert isinstance(annotation['annotation']['end'], int)
+                    assert isinstance(annotation['annotation']['created'], str)
+                    assert isinstance(annotation['annotation']['category']['unsure'], bool)
+                    assert isinstance(annotation['annotation']['category']['guid'], str)
+                    index = obs['pdf_text'][k][annotation['annotation']['start']:annotation['annotation']['end']]
+                    content = annotation['annotation']['content']
+                    assert index == content
+                except:
+                    annotation_errors = annotation_errors + 1
             k = k + 1
     except Exception as e:
         error_count = error_count + 1
@@ -39,7 +42,7 @@ for i, obs in enumerate(data):
         print(traceback.format_exc())
         print(f'{e}')
 
-print(f'Antal fejl: {error_count}')
+
 
 bilou_error_count = 0
 for i, obs in enumerate(data_bilou):
@@ -50,4 +53,6 @@ for i, obs in enumerate(data_bilou):
         print(f'Bilou data med nummer {i} fejlede')
         print(traceback.format_exc())
         print(f'{e}')
+
+print(f'Antal dokumenter med fejl: {error_count}, Antal annotations med fejl: {annotation_errors}')
 print(f'Antal bilou fejl: {bilou_error_count}')
