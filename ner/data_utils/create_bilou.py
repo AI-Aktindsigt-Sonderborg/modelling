1430dc686b544d30ae6fc6b82dfaef60e6a33af2
kshll6 <kshll6@hotmail.com>
2023-05-11 16:34:42 +0200

    update ner validation

--- ner/data_utils/create_bilou.py
+++ ner/data_utils/create_bilou.py
@@ -4,28 +4,34 @@ from ner.local_constants import DATA_DIR
 from shared.utils.helpers import read_json_lines
 
 args = sys.argv[1:]
-args.append("non_sensitive_output")
-args.append("non_sensitive_bilou_output_2")
+args.append("origin")
+args.append("bilou")
 
 data = read_json_lines(input_dir=DATA_DIR, filename=args[0])
 bilou = read_json_lines(input_dir=DATA_DIR, filename=args[1])
 
 entity_data = []
 for i, obs in enumerate(data):
-    page_num
-    k = 0
-    for page_num in list(obs['text_annotation']):
-        for j, annotation in enumerate(obs['text_annotation'][page_num]):
-            index = obs['pdf_text'][k][annotation['annotation']['start']:annotation['annotation']['end']]
+    for k, pdf_text in enumerate(obs['pdf_text']):
+        index_diff = 0
+        pdf_altered = pdf_text
+        page_num = f'page_no:{k+1}'
+        current_page_annotations = obs['text_annotation'][page_num]
+
+        for j, annotation in enumerate(current_page_annotations):
+            index = pdf_altered[annotation['annotation']['start'] + index_diff:annotation['annotation']['end'] + index_diff]
             content = annotation['annotation']['content']
             if not index == content:
                 print()
             else:
-                list_content = content.split(" ")
+                pdf_altered = pdf_altered[:annotation['annotation']['start']] + \
+                              annotation['annotation']['annotation'] + pdf_altered[annotation['annotation']['end']:]
 
+                index_diff = len(pdf_altered) - len(pdf_text)
 
+                list_content = content.split()
                 if len(list_content) == 1:
-                    anon_content = annotation['']
+                    # anon_content = annotation['']
                     print("prut")
                 elif len(list_content) == 2:
                     print("prut")
--- ner/data_utils/ner_input_validation.py
+++ ner/data_utils/ner_input_validation.py
@@ -14,55 +14,65 @@ data_bilou = read_json_lines(DATA_DIR, filename_bilou)
 error_count = 0
 annotation_errors = 0
 annotation_counter = 0
+j = 0
 for i, obs in enumerate(data):
     try:
         assert isinstance(obs['id'], int)
         assert isinstance(obs['process_id'], str)
         assert isinstance(obs['display_name'], str)
-        #assert isinstance(obs['file_extension'], str)
-        for page in obs['pdf_text']:
-            assert isinstance(page, str)
-        k = 0
-        for page_num in list(obs['text_annotation']):
-            for j, annotation in enumerate(obs['text_annotation'][page_num]):
+        for k, pdf_text in enumerate(obs['pdf_text']):
+            page_num = f'page_no:{k + 1}'
+            assert isinstance(pdf_text, str)
+            current_page_annotations = obs['text_annotation'][page_num]
+
+            for j, annotation in enumerate(current_page_annotations):
                 try:
                     assert isinstance(annotation, dict)
                     assert isinstance(annotation['annotation']['id'], int)
                     assert isinstance(annotation['annotation']['state'], str)
                     assert isinstance(annotation['annotation']['content'], str)
-                    assert isinstance(annotation['annotation']['annotation'], str)
+                    assert isinstance(annotation['annotation']['annotation'],
+                                      str)
                     assert isinstance(annotation['annotation']['start'], int)
                     assert isinstance(annotation['annotation']['end'], int)
                     assert isinstance(annotation['annotation']['created'], str)
-                    assert isinstance(annotation['annotation']['category']['unsure'], bool)
-                    assert isinstance(annotation['annotation']['category']['guid'], str)
-                    index = obs['pdf_text'][k][annotation['annotation']['start']:annotation['annotation']['end']]
+                    assert isinstance(
+                        annotation['annotation']['category']['unsure'], bool)
+                    assert isinstance(
+                        annotation['annotation']['category']['guid'], str)
+                    index = pdf_text[annotation['annotation']['start']:
+                                     annotation['annotation']['end']]
                     content = annotation['annotation']['content']
                     assert index == content
+
                 except:
-                    print(f"data med linjenummer {i+1} med id {obs['id']} fejlede paa annotation nummer {j}.")
+                    print(
+                        f"data med linjenummer {i + 1} med id {obs['id']} fejlede paa annotation nummer {j}.")
                     print(traceback.format_exc())
                     annotation_errors = annotation_errors + 1
             annotation_counter = annotation_counter + j
-            k = k + 1
+
     except Exception as e:
         error_count = error_count + 1
-        print(f"data med linjenummer {i+1} med id {obs['id']} fejlede paa input validering.")
+        print(
+            f"data med linjenummer {i + 1} med id {obs['id']} fejlede paa input validering.")
         print(f'page_num: {page_num}, annotation_num: {j}')
         print(traceback.format_exc())
         print(f'{e}')
-
 
 bilou_error_count = 0
 for i, obs in enumerate(data_bilou):
     if len(obs['words']) != len(obs['tags']):
         bilou_error_count = bilou_error_count + 1
-        print(f'len(Words) != len(tags): Bilou data med linjenummer {i+1} fejlede')
+        print(
+            f'len(Words) != len(tags): Bilou data med linjenummer {i + 1} fejlede')
     if obs['faulty_annotation']:
         bilou_error_count = bilou_error_count + 1
-        print(f'faulty_annotation fejl: Bilou data med linjenummer {i+1} fejlede')
+        print(
+            f'faulty_annotation fejl: Bilou data med linjenummer {i + 1} fejlede')
 
-
-print(f'Antal dokumenter med generelle fejl: {error_count}, antal dokumenter i alt: {len(data)}')
-print(f'Antal annotations med fejl: {annotation_errors} (annotations i alt: {annotation_counter})')
+print(
+    f'Antal dokumenter med generelle fejl: {error_count}, antal dokumenter i alt: {len(data)}')
+print(
+    f'Antal annotations med fejl: {annotation_errors} (annotations i alt: {annotation_counter})')
 print(f'Antal bilou fejl: {bilou_error_count}, bilou i alt: {len(data_bilou)}')
