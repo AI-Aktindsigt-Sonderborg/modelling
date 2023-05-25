b40e61e87b872a99388efc030098e37b1b950e8b
kshll6 <kshll6@hotmail.com>
2023-05-25 14:38:46 +0200

    update bilou error handling

--- ner/data_utils/bilou_val.py
+++ ner/data_utils/bilou_val.py
@@ -1,8 +1,9 @@
 import sys
-
+from itertools import groupby
 from shared.utils.helpers import read_json_lines
 from ner.local_constants import DATA_DIR
 import traceback
+from itertools import groupby
 
 args = sys.argv[1:]
 
@@ -17,14 +18,24 @@ annotation_counter = 0
 j = 0
 
 bilou_error_count = 0
+total_entities = []
 for i, obs in enumerate(data_bilou):
-    print()
+
     if len(obs['words']) != len(obs['tags']):
         bilou_error_count = bilou_error_count + 1
         print(f'len(Words) != len(tags): Bilou data med linjenummer {i + 1} fejlede')
-    if obs['faulty_annotation']:
-        bilou_error_count = bilou_error_count + 1
-        print(f'faulty_annotation fejl: Bilou data med linjenummer {i + 1} fejlede')
+        print(obs['sentence'])
+        if obs['faulty_annotation']:
+            bilou_error_count = bilou_error_count + 1
+            print(f'faulty_annotation fejl: Bilou data med linjenummer {i + 1} fejlede')
+    else:
+        total_entities.extend(obs['entities'])
+
+total_entities.sort()
+grouped = [list(group) for key, group in groupby(total_entities)]
+
+for group in grouped:
+    print(f"Entitet: {group[0]} - antal: {len(group)}")
 
 print(f"Total sentences: {len(data_bilou)}")
 
