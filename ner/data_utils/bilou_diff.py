309d0a252eebed009c4f0d745656ef79fbda3c10
kshll6 <kshll6@hotmail.com>
2023-06-02 13:51:26 +0200

    load sentence splitter

--- ner/data_utils/bilou_diff.py
+++ ner/data_utils/bilou_diff.py
@@ -3,9 +3,6 @@ import sys
 from ner.local_constants import DATA_DIR
 from shared.utils.helpers import read_json_lines
 
-
-
-
 args: list = sys.argv[1:]
 
 bilou_1 = read_json_lines(DATA_DIR, args[0])
@@ -23,13 +20,14 @@ for i, obs in enumerate(bilou_2):
     bilou_2_tokens.append(obs['tokens'])
     bilou_2_tags.append(obs['tags'])
 
-bilou_2_only = list(set(bilou_2_tokens) - set(bilou_1_tokens))
-bilou_1_only = list(set(bilou_1_tokens) - set(bilou_2_tokens))
+bilou_2_only = list(set(tuple(sublist) for sublist in bilou_2_tokens) - set(tuple(sublist) for sublist in bilou_1_tokens))
+bilou_1_only = list(set(tuple(sublist) for sublist in bilou_1_tokens) - set(tuple(sublist) for sublist in bilou_2_tokens))
 
-if args[2] == 2:
+
+if args[2] == "2":
     for element in bilou_2_only:
         print(element)
 
-if args[2] == 1:
+if args[2] == "1":
     for element in bilou_1_only:
         print(element)
