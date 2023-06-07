c05be35589ac3de2d5cbffe047ee4464a5472410
kshll6 <kshll6@hotmail.com>
2023-06-07 12:33:04 +0200

    load sentence splitter

--- shared/modelling_utils/modelling.py
+++ shared/modelling_utils/modelling.py
@@ -458,9 +458,8 @@ class Modelling:
                               data=dataclasses.asdict(eval_score))
             eval_scores.append(eval_score)
 
-            if self.args.save_steps is not None and (
-                step > self.args.freeze_layers_n_steps and
-                (step % self.args.save_steps == 0)):
+            # Todo: fix step > self.args.freeze_layers_n_steps and
+            if self.args.save_steps is not None and (step % self.args.save_steps == 0):
                 _, save_best_model = get_metrics(
                     eval_scores=eval_scores,
                     eval_metrics=self.args.eval_metrics)
