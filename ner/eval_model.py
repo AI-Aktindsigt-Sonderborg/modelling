13ef03e7149a206bdf9319929b2e0c74c3a5dd76
Kasper Schjødt-Hansen <alvenirvpnks@skpwgpu01.sonderborg.alssok.net>
2023-08-04 10:32:30 +0200

    clean up eval

--- ner/eval_model.py
+++ ner/eval_model.py
@@ -19,7 +19,7 @@ args.evaluate_during_training = False
 args.load_alvenir_pretrained = True
 args.differential_privacy = False
 args.test = True
-args.eval_batch_size = 1
+# args.eval_batch_size = 1
 args.normalize_conf = "true"
 # args.max_length = 512
 # args.test_data = "bilou_val.jsonl"
@@ -29,77 +29,9 @@ modelling = NERModelling(args)
 
 modelling.load_data(train=False, test=args.test)
 
-# for i in range(len(modelling.data.test)):
-# modelling.data.test[i]["ner_tags"] = [
-# modelling.label2id[tag] for tag in modelling.data.test[i]["tags"]
-#    ]
-
-
-def tokenize_and_align_labels_for_dataset(dataset, tokenizer):
-    def tokenize_and_align_labels(examples):
-        tokenized_inputs = tokenizer(
-            examples["tokens"],
-            truncation=True,
-            is_split_into_words=True,
-            padding="max_length",
-            max_length=args.max_length,
-        )
-
-        labels = []
-        labels_tokenized = []
-
-        for i, label in enumerate(examples["tags"]):
-            label = [modelling.label2id[lab] for lab in label]
-            label_tokenized = tokenizer.tokenize(" ".join(examples["tokens"][i]))
-            label_tokenized.insert(0, "-100")
-            label_tokenized.append("-100")
-
-            # Map tokens to their respective word.
-            word_ids = tokenized_inputs.word_ids(batch_index=i)
-
-            previous_word_idx = None
-            label_ids = []
-            for word_idx in word_ids:  # Set the special tokens to -100.
-                if word_idx is None:
-                    label_ids.append(-100)
-                elif word_idx != previous_word_idx:
-                    # Only label the first token of a given word.
-                    label_ids.append(label[word_idx])
-                else:
-                    label_ids.append(-100)
-                previous_word_idx = word_idx
-
-            labels.append(label_ids)
-            labels_tokenized.append(label_tokenized)
-
-        # ToDo: labels tokenized instead of labels?
-        tokenized_inputs["labels"] = labels
-        # tokenized_inputs["labels_tokenized"] = labels_tokenized
-
-        return tokenized_inputs
-
-    tokenized_dataset = dataset.map(
-        tokenize_and_align_labels, batched=True, remove_columns=dataset.column_names
-    )
-    tokenized_dataset.set_format("torch")
-
-    #    tokenized_dataset_new = tokenized_dataset.remove_columns(
-    #       ["ner_tags", "labels_tokenized", "tokens"]
-    #  )
-
-    return tokenized_dataset
-
-
-tokenized = tokenize_and_align_labels_for_dataset(
-    dataset=modelling.data.test, tokenizer=modelling.tokenizer
-)
-
-wrapped = DatasetWrapper(tokenized)
-
-
 wrapped, test_loader = create_data_loader(
-    # data_wrapped=modelling.tokenize_and_wrap_data(modelling.data.test),
-    data_wrapped=wrapped,
+    data_wrapped=modelling.tokenize_and_wrap_data(modelling.data.test),
+    # data_wrapped=wrapped,
     data_collator=modelling.data_collator,
     batch_size=modelling.args.eval_batch_size,
     shuffle=False,
