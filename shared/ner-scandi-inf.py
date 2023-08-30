4ba85ac6f528c50c0d5b358495a1d930e1244f63
kshll6 <kshll6@hotmail.com>
2023-08-30 16:35:51 +0200

    add math

--- shared/ner-scandi-inf.py
+++ shared/ner-scandi-inf.py
@@ -6,8 +6,8 @@ ner = pipeline(task='ner',
                model='../ner/models/24-akt-mlm-BIO/best_model',
                aggregation_strategy='first')
 
-sentence = 'Kasper Schjødt-Hansen er medejer i virksomheden Alvenir Aps og har ofte ekstrem hovedpine.' \
-           ' Han bor på Blegdamsvej 74, 2100 København Ø. ' \
+sentence = 'Kasper Schjødt-Hansen er medarbejder i virksomheden Alvenir Aps og har ofte ekstrem hovedpine.' \
+           ' Han bor på Blegdamsvej 85, 2100 København Ø som ligger i københavns kommune.' \
            'Hans tlf nummer er 12345560 og han er fra Danmark.'
 
 
