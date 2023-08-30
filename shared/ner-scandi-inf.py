6dd77a1593f47ecac60e62db3b4b2195c756891c
kshll6 <kshll6@hotmail.com>
2023-08-30 16:25:05 +0200

    add math

--- shared/ner-scandi-inf.py
+++ shared/ner-scandi-inf.py
@@ -6,13 +6,20 @@ ner = pipeline(task='ner',
                model='../ner/models/24-akt-mlm-BIO/best_model',
                aggregation_strategy='first')
 
-sentence = 'Kasper Schjødt-Hansen er medejer i virksomheden Alvenir Aps og har ofte ekstrem hovedpine. Han bor på Blegdamsvej 74, 2100 København Ø'
-print(sentence)
+sentence = 'Kasper Schjødt-Hansen er medejer i virksomheden Alvenir Aps og har ofte ekstrem hovedpine.' \
+           'Han bor på Blegdamsvej 74, 2100 København Ø. ' \
+           'Hans tlf nummer er 12345560 og han er fra danmark.'
+
+
 
 result = ner(sentence)
+print("Input sætning:")
+print(sentence)
+
 
 pd_result = pd.DataFrame.from_records(result)
 
+print("Entiteter: ")
 print(pd_result)
 
 print()
