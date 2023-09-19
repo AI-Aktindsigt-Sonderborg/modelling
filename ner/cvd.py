2e0a02d29b2796a38287293291b6134508b448f2
kshll6 <kshll6@hotmail.com>
2023-09-19 16:38:20 +0200

    update ner vejen script with comments

--- ner/cvd.py
+++ ner/cvd.py
@@ -1,3 +1,23 @@
+"""
+Script to generate 'model-ready' test data from raw vejen data
+
+origin file must be placed in folder '../modelling/ner/data'
+
+Origin file must have '.jsonl' extension
+
+Navigate to project root folder and run script via terminal:
+    python -m ner.CVD --origin_input_file <origin_file_name>
+
+Example run with origin file name 'origin_vejen.jsonl':
+    python -m ner.CVD --origin_input_file origin_vejen
+
+Important files created from script placed in '../modelling/ner/data':
+    bio_PLAHOKT.jsonl --> file to test model excluding CPR and Forbrydelse
+    bio_PLAHOKTFC.jsonl --> file to test model including CPR and Forbrydelse
+
+"""
+
+
 from ner.data_utils.custom_dataclasses import DataPrepConstants
 from ner.data_utils.data_prep_input_args import DataPrepArgParser
 from ner.data_utils.prep_data import NERDataPreprocessing
