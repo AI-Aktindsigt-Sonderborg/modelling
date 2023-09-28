4f6818b63396ff11e660430745e166d6a2dd8ede
Kasper Schjødt-Hansen <alvenirvpnks@skpwgpu01.sonderborg.alssok.net>
2023-09-28 13:20:57 +0200

    fix ner inference

--- ner/inference.py
+++ ner/inference.py
@@ -1,12 +1,14 @@
 from transformers import pipeline
 import pandas as pd
 
-ner = pipeline(task='ner',
-               model='ner/models/sas-ner/best_model',
-           aggregation_strategy='first')
+ner = pipeline(
+    task="ner", model="ner/models/sas-ner/best_model", aggregation_strategy="first"
+)
 
-sentence = 'Kasper Schjødt-Hansen er medarbejder i virksomheden Alvenir Aps og har ofte ekstrem hovedpine. Han bor på Blegdamsvej 85, 2100 København Ø som ligger i Københavns Kommune.' \
-   ' Hans tlf nummer er 12345560 og han er fra Danmark. Blegamsvej er tæt på Fælledparken.'
+sentence = (
+    "Kasper Schjødt-Hansen er medarbejder i virksomheden Alvenir Aps og har ofte ekstrem hovedpine. Han bor på Blegdamsvej 85, 2100 København Ø som ligger i Københavns Kommune."
+    " Hans tlf nummer er 12345560 og han er fra Danmark. Blegamsvej er tæt på Fælledparken."
+)
 
 
 result = ner(sentence)
